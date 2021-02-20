15625c2f3d06d7696080c041c97a72a9314565a1
Steven Calderon <scalderon1504@gmail.com>
2021-02-19 22:39:40 -0500

    Initial commit

--- src/main/java/co/edu/usbcali/demo/domain/Admin.java
+++ src/main/java/co/edu/usbcali/demo/domain/Admin.java
@@ -13,12 +13,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
-* @author Zathura Code Generator Version 9.0 http://zathuracode.org/
-* www.zathuracode.org
-* 
-*/
+ * @author Zathura Code Generator Version 9.0 http://zathuracode.org/
+ *         www.zathuracode.org
+ * 
+ */
 @Entity
-@Table ( name="admin", schema="public" )
+@Table(name = "admin", schema = "public")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -26,51 +26,48 @@ public class Admin implements java.io.Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@Column(name = "admin_identification", unique = true, nullable = false)
+	@NotNull
+	private String adminIdentification;
 
-			@Id
-	    @Column(name="admin_identification", unique=true, nullable=false)
-		@NotNull
-		private String adminIdentification;
-		
-	
-	    
-						@NotNull
-						@NotEmpty
-			@Size(max=255)
-							@Column(name="email"  , nullable=false  )
-		private String email;	
-    				@Column(name="first_name"   )
-		private String firstName;	
-    					@NotNull
-						@NotEmpty
-			@Size(max=20)
-							@Column(name="identification_type"  , nullable=false  )
-		private String identificationType;	
-    					@NotNull
-						@NotEmpty
-			@Size(max=120)
-							@Column(name="last_name"  , nullable=false  )
-		private String lastName;	
-    					@NotNull
-						@NotEmpty
-			@Size(max=40)
-							@Column(name="last_name2"  , nullable=false  )
-		private String lastName2;	
-    					@NotNull
-						@NotEmpty
-			@Size(max=255)
-							@Column(name="password"  , nullable=false  )
-		private String password;	
-    					@NotNull
-						@NotEmpty
-			@Size(max=1)
-							@Column(name="state"  , nullable=false  )
-		private String state;	
-    					@NotNull
-							@Column(name="valid_register"  , nullable=false  )
-		private Date validRegister;	
-        
-		@OneToMany(fetch=FetchType.LAZY, mappedBy="admin")
-		private List<Doctor> doctors = new ArrayList<>();	
-        
-}	
+	@NotNull
+	@NotEmpty
+	@Size(max = 255)
+	@Column(name = "email", nullable = false)
+	private String email;
+	@Column(name = "first_name")
+	private String firstName;
+	@NotNull
+	@NotEmpty
+	@Size(max = 20)
+	@Column(name = "identification_type", nullable = false)
+	private String identificationType;
+	@NotNull
+	@NotEmpty
+	@Size(max = 120)
+	@Column(name = "last_name", nullable = false)
+	private String lastName;
+	@NotNull
+	@NotEmpty
+	@Size(max = 40)
+	@Column(name = "last_name2", nullable = false)
+	private String lastName2;
+	@NotNull
+	@NotEmpty
+	@Size(max = 255)
+	@Column(name = "password", nullable = false)
+	private String password;
+	@NotNull
+	@NotEmpty
+	@Size(max = 1)
+	@Column(name = "state", nullable = false)
+	private String state;
+	@NotNull
+	@Column(name = "valid_register", nullable = false)
+	private Date validRegister;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "admin")
+	private List<Doctor> doctors = new ArrayList<>();
+
+}
