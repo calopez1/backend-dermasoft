bf23ecdffafdf1e866efb10dbc3cc4e63ecd8e4f
Steven Calderon <scalderon1504@gmail.com>
2021-02-21 14:58:47 -0500

    Permitir null

--- src/main/java/co/edu/usbcali/demo/dto/PatientDTO.java
+++ src/main/java/co/edu/usbcali/demo/dto/PatientDTO.java
@@ -1,16 +1,15 @@
 package co.edu.usbcali.demo.dto;
 
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.io.Serializable;
-
-import java.sql.*;
-
-import java.util.Date;
-
-import javax.validation.constraints.*;
 
 
 /**
@@ -23,22 +22,15 @@ import javax.validation.constraints.*;
 @NoArgsConstructor
 public class PatientDTO implements Serializable {
     private static final long serialVersionUID = 1L;
-    @NotNull
-    @NotEmpty
     @Size(max = 20)
     private String approved;
-    @NotNull
     private Date birthdate;
     @NotNull
     @NotEmpty
     @Size(max = 255)
     private String email;
-    @NotNull
-    @NotEmpty
     @Size(max = 80)
     private String firstName;
-    @NotNull
-    @NotEmpty
     @Size(max = 1)
     private String gender;
     private Integer height;
@@ -47,20 +39,12 @@ public class PatientDTO implements Serializable {
     @Size(max = 40)
     private String identificationType;
     private String image;
-    @NotNull
-    @NotEmpty
     @Size(max = 120)
     private String lastName;
-    @NotNull
-    @NotEmpty
     @Size(max = 40)
     private String lastName2;
-    @NotNull
-    @NotEmpty
     @Size(max = 20)
     private String maritalStatus;
-    @NotNull
-    @NotEmpty
     @Size(max = 50)
     private String ocupation;
     @NotNull
@@ -71,18 +55,11 @@ public class PatientDTO implements Serializable {
     @NotEmpty
     @Size(max = 20)
     private String patientIdentification;
-    @NotNull
-    @NotEmpty
     @Size(max = 20)
     private String phone;
-    @NotNull
-    @NotEmpty
     @Size(max = 1)
     private String state;
-    @NotNull
     private Date validRegister;
-    @NotNull
-    @NotEmpty
     @Size(max = 1)
     private String verified;
     private Integer weight;
